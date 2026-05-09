86935d5d7f783b041d94dd0c62fc78571f95150a
Abolaji Ayeni <ayenibolaji07@gmail.com>
2026-05-09 12:44:04 +0100

    configure Dio client with base URL, timeouts, and error interceptor

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,6 +2,9 @@ class AppConstants {
   AppConstants._();
   static const String baseUrl = 'https://rickandmortyapi.com/api';
   static const String locationEndpoint = '/location';
+  static const int maxResidentsShown = 6;
+  static const Duration connectTimeout = Duration(seconds: 10);
+  static const Duration receiveTimeout = Duration(seconds: 15);
 }
 
 class AppStrings {
